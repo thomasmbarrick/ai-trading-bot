c0fb1871078a7dbe8f0677e112a748ee262be9a8
Thomas Barrick <thomasmbarrick@gmail.com>
2024-07-19 16:14:36 +0100

    news

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime as dt 
+from alpaca_trade_api import REST
+from timedelta import Timedelta
 
 API_KEY = "PKE2VBNLWVIB2MEPHAMI"
 API_SECRET = "yBbvQY9rar69pQjeamfFiJtikPbgyk1A0lKytL6K"
@@ -21,18 +23,35 @@ class MLStrategy(Strategy):
         self.sleeptime = "24H"
         self.last_trade = None
         self.cash_at_risk = cash_at_risk
+        self.api = REST(base_url=BASE_URL,
+                        key_id=API_KEY,
+                        secret_key=API_SECRET)
 
     def position_sizing(self):
         cash = self.get_cash()
         last_price = self.get_last_price(self.symbol)
         quantity = cash * self.cash_at_risk / last_price
         return cash, last_price, quantity
-        
+    
+    def get_dates(self):
+        today = self.get_datetime()
+        three_days_prior = today - Timedelta(days=3)
+        return today.strftime("%Y-%m-%d"), three_days_prior.strftime("%Y-%m-%d")
+    
+    def get_news(self):
+        today, three_days_prior = self.get_dates()
+        news = self.api.get_news(symbol=self.symbol, 
+                                 start=three_days_prior,
+                                 end=today)
+        news = {event.__dict__["_raw"]["headline"] for event in news}
+        return news
         
     def on_trading_iteration(self):
         cash, last_price, quantity = self.position_sizing()
         if cash > last_price:
             if self.last_trade is None:
+                news = self.get_news()
+                print(news)
                 order = self.create_order(
                     self.symbol,
                     quantity,
